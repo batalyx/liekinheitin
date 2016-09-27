d0fa61d20f845a19087ba47e8f697d623ecf6d88
Jonne Itkonen <batalyx@users.noreply.github.com>
2016-09-27 10:48:04 +0300

    Eka, toimiva versio

--- Liekinheitin/GameScene.swift
+++ Liekinheitin/GameScene.swift
@@ -9,77 +9,42 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    private var label : SKNode?
+    private var poppa : SKNode?
+    private var pökäAlku: CGPoint?
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        self.physicsWorld.contactDelegate = self
+
+        self.label = self.childNode(withName: "//pökät")
+        self.pökäAlku = self.label?.position
+
+        self.poppa = self.childNode(withName: "//poppa")
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let hyp = SKAction.moveBy(x: -50, y: 200, duration: TimeInterval(0.1))
+        let takas = SKAction.move(to: pökäAlku!, duration: TimeInterval(1))
+        let auts = SKAction.sequence([hyp, takas])
+
+        if contact.bodyA.categoryBitMask == 0x02 {        // A = pökät, B = poppa
+            contact.bodyB.node?.run(auts)
+        } else if contact.bodyB.categoryBitMask == 0x02 { // A = poppa, B = pökät
+            contact.bodyA.node?.run(auts)
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
+
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        case 124:
+            if var x = self.label?.position.x {
+                x += 3
+                if x > 10 { x = -220 }
+                self.label?.position.x = x
             }
+            break
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
@@ -87,6 +52,5 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
