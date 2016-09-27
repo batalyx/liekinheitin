9a6cfb5c12892f3221ce633074005b55f6b7bd0c
Jonne Itkonen <batalyx@users.noreply.github.com>
2016-09-27 08:48:58 +0300

    Initial Commit

---
+++ Liekinheitin/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Liekinheitin
+//
+//  Created by Jonne Itkonen on 27.9.2016.
+//  Copyright © 2016 Jonne Itkonen. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Liekinheitin/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Liekinheitin
+//
+//  Created by Jonne Itkonen on 27.9.2016.
+//  Copyright © 2016 Jonne Itkonen. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
